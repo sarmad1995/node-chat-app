7e0b5b4fbea49914777ae11e32fe085ea31ad9f0
Sarmad Aijaz <sarmad@Sarmads-MacBook-Air.local>
2018-04-20 16:46:54 +0530

    Add broadcasts

--- server/server.js
+++ server/server.js
@@ -11,8 +11,24 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (newMessage) => {
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from: newMessage.from,
       text: newMessage.text,
       createdAt: new Date().getTime()
